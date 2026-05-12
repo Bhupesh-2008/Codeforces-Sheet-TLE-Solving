fb8d9f107a058bee40d7fd6fb19700565a690160
Bhupesh-2008 <bgarg.20062008@gmail.com>
2026-05-12 12:00:13 +0530

    day 3

--- 800 Rating problem/ForbiddenInteger.cpp
+++ 800 Rating problem/ForbiddenInteger.cpp
@@ -7,64 +7,46 @@ using vi = vector<int>;
 using vll = vector<ll>;
 using pii = pair<int, int>;
 
-#define fast_io                       \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL);
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 #define all(x) (x).begin(), (x).end()
 #define pb push_back
 #define sz(x) ((int)(x).size())
 
-int main()
-{
+int main() {
     fast_io; // Optimization for competitive programming
 
     int t = 0;
-    cin >> t;
-    while (t--)
-    {
-        int k, n, x;
-        cin >> n >> k >> x;
+    cin >> t; 
+    while (t--) {
+        int k,n,x;
+        cin>>n>>k>>x;
         vi v;
-        for (int i = 1; i < k + 1; i++)
-        {
-            if (i != x)
-                v.push_back(i);
+        for(int i=1; i<k+1;i++){
+            if(i!=x) v.push_back(i);
         }
-        if (x != 1)
-        {
-            cout << "YES\n";
-            cout << n << "\n";
-            for (int i = 0; i < n; i++)
-            {
-                cout << "1 ";
-            }
-            cout << "\n";
+        if(x!=1) {
+            cout<<"YES\n";
+            cout<<n<<"\n";
+            for(int i=0; i<n; i++){
+                cout<<"1 ";
+            }cout<<"\n";
         }
-        else
-        {
-            if (k == 1 || (k == 2 && n % 2 != 0))
-            {
-                cout << "NO\n";
+        else{
+            if(k==1 || (k==2 && n%2!=0)){
+                cout<<"NO\n";
             }
-            else if (n % 2 == 0)
-            {
-                cout << "YES\n";
-                cout << n / 2 << "\n";
-                for (int i = 0; i < n / 2; i++)
-                    cout << "2 ";
-                cout << "\n";
-            }
-            else
-            {
-                cout << "YES\n";
-                cout << 1 + (n - 3) / 2 << "\n";
-                cout << "3 ";
-                for (int i = 0; i < (n - 3) / 2; i++)
-                {
-                    cout << "2 ";
-                }
-                cout << "\n";
+            else if(n%2==0 ){
+                cout<<"YES\n";
+                cout<<n/2<<"\n";
+                for(int i=0; i<n/2; i++)cout<<"2 ";
+                cout<<"\n";
+            }else{
+                cout<<"YES\n";
+                cout<<1+(n-3)/2<<"\n";
+                cout<<"3 ";
+                for(int i=0; i<(n-3)/2;i++){
+                    cout<<"2 ";
+                }cout<<"\n";
             }
         }
     }
