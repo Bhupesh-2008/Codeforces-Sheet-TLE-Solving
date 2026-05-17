0d772ad87e87fa437eca3e52c3232708b9c9def1
Bhupesh-2008 <217087566+Bhupesh-2008@users.noreply.github.com.>
2026-05-17 12:10:38 +0530

    TwoPermutations

---
+++ 800 Rating problem/TwoPermutations.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shorthand for common types and containers
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) ((int)(x).size())
+
+int main() {
+    fast_io; // Optimization for competitive programming
+
+    int t = 0;
+    cin >> t; 
+    while (t--) {
+        int n,a,b;
+        cin>>n>>a>>b;
+        if(a==b && a==n) cout<<"Yes\n";
+        else if(n-a-b>=2) cout<<"Yes\n";
+        else cout<<"No\n";
+    }
+
+    return 0;
+}
