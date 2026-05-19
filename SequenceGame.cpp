8a1dcb26f0117220ab41c71bb0639880c10c5a4a
Bhupesh-2008 <217087566+Bhupesh-2008@users.noreply.github.com.>
2026-05-19 21:39:25 +0530

    sequence game

---
+++ SequenceGame.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shorthand for common types and containers
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) ((int)(x).size())
+
+int main() {
+    fast_io; // Optimization for competitive programming
+
+    int t = 0;
+    cin >> t; 
+    while (t--) {
+        ll n; cin>>n;
+        vll v(n);
+        for(ll i=0; i<n; i++) cin>>v[i];
+
+        vll ans;
+        ans.push_back(v[0]);
+        for(int i=1; i<n; i++){
+            if(v[i-1]<=v[i])ans.push_back(v[i]);
+            else{
+                ans.push_back(v[i]);
+                ans.push_back(v[i]);
+            }
+        }
+        cout<<ans.size()<<"\n";
+        for(ll i=0; i<ans.size(); i++){
+            cout<<ans[i]<<" ";
+        }cout<<"\n";
+    }
+
+    return 0;
+}
