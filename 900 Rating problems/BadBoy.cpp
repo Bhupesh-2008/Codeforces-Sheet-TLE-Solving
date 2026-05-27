52fbb16be71f6a348081304f73aca3f5c9738e35
Bhupesh-2008 <217087566+Bhupesh-2008@users.noreply.github.com.>
2026-05-27 10:53:26 +0530

    BadBoy

---
+++ 900 Rating problems/BadBoy.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shorthand for common types and containers
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) ((int)(x).size())
+
+int main() {
+    fast_io; // Optimization for competitive programming
+
+    int t = 0;
+    cin >> t; 
+    while (t--) {
+        ll n,m,i,j;
+        cin>>n>>m>>i>>j;
+
+        cout<<1<<" "<<1<<" "<<n<<" "<<m<<"\n";
+    }
+
+    return 0;
+} 
