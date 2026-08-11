2468327a48332a167de2cff916d6194d5d6b48d9
Bhupesh-2008 <217087566+Bhupesh-2008@users.noreply.github.com.>
2026-08-11 09:35:04 +0530

    minimum lcm

---
+++ 1000 Rating problems/MinimumLCM.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shorthand for common types and containers
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll> ;
+using pii = pair<int, int>;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) ((int)(x).size())
+
+int main() {
+    fast_io; // Optimization for competitive programming
+
+    int t = 0;
+    cin >> t; 
+    while (t--) {
+        ll n=0;
+        cin>>n;
+        ll a=1,b=0;
+        for(ll i=2; i*i<=n; i++){
+            if(n%i==0){
+                a=n/i;
+                break;
+            }
+        }
+        b= n-a;
+
+        cout<<a<<" "<<b <<"\n";
+    }
+
+    return 0;
+}
